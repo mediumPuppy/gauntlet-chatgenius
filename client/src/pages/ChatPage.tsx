33559dd0c9580f5f05fc0b1e761b5ffb5dff4f48
jefferson gauntlet <jefferson.lambert@gauntletai.com>
2025-01-10 22:22:40 -0700

    nothing

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -175,7 +175,7 @@ const ChatPageContent: React.FC = memo(() => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                 </svg>
                 <h3 className="text-xl font-semibold mb-2">Welcome to Chat</h3>
-                <p className="text-center">Select a channel or direct message from the sidebar to start chatting</p>
+                <p className="text-center">Select a channel or direct message from the sidebar to start a chatting</p>
               </div>
             )}
             {shouldShowChat && (
